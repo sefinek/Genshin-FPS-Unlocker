8752170e5e5db9b40c84182147f8bcd690475f06
Sefinek <sefinek5916@gmail.com>
2022-11-18 09:32:04 +0100

    Update

--- unlockfps_clr/Managed.h
+++ unlockfps_clr/Managed.h
@@ -10,13 +10,13 @@ using namespace System::Runtime::InteropServices;
 
 namespace Managed
 {
-	// start game with launch options
+	// Start game with launch options
 	bool StartGame(Settings^ settings);
 
-	// attempt to find game path through registry
+	// Attempt to find game path through registry
 	List<String^>^ TryResolveGamePath();
 
-	// get game path while the game is running
+	// Get game path while the game is running
 	String^ TryGetGamePath();
 
 	void InjectDLLs(List<String^>^ paths);
