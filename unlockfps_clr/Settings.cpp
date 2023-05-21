2a303c7591646a7aa3f8ab6915eb812f455df90a
sefinek24 <sefinek5916@gmail.com>
2023-05-21 04:19:18 +0200

    bnjkhkhjlgjkhghkjl

--- unlockfps_clr/Settings.cpp
+++ unlockfps_clr/Settings.cpp
@@ -1,68 +1,68 @@
 #include "Settings.h"
 
 using namespace System;
-using namespace System::IO;
-using namespace System::Text;
-using namespace System::Text::Json;
-using namespace System::Windows::Forms;
+using namespace IO;
+using namespace Text;
+using namespace Json;
+using namespace Windows::Forms;
 
 Settings::Settings()
 {
-    
 }
 
 Settings^ Settings::InitializeDefaults()
 {
-    ConfigVersion = "1.0.0";
-    ConfigDate = "02.12.2022";
+	ConfigVersion = "1.1.1";
+	ConfigDate = "18.04.2023";
 
-    GamePath = "";
+	GamePath = "";
 
-    AutoStart = false;
-    AutoDisableVSync = true;
-    PopupWindow = false;
-    Fullscreen = false;
-    UseCustomRes = false;
-    IsExclusiveFullscreen = false;
-    UsePowerSave = false;
+	AutoStart = false;
+	AutoClose = true;
+	AutoDisableVSync = true;
+	PopupWindow = false;
+	Fullscreen = true;
+	UseCustomRes = false;
+	IsExclusiveFullscreen = false;
+	UsePowerSave = false;
 
-    FPSTarget = 60;
-    CustomResX = 1920;
-    CustomResY = 1080;
-    Priority = 3;
+	FPSTarget = 60;
+	CustomResX = 1920;
+	CustomResY = 1080;
+	Priority = 3;
 
-    DllList = gcnew List<String^>();
+	DllList = gcnew List<String^>();
 
-    return this;
+	return this;
 }
 
 void Settings::Save()
 {
-    auto options = gcnew JsonSerializerOptions();
-    options->WriteIndented = true;
+	auto options = gcnew JsonSerializerOptions();
+	options->WriteIndented = true;
 
-    auto serialized = JsonSerializer::Serialize(this, options);
-    File::WriteAllText(ConfigPath, serialized, Encoding::UTF8);
+	auto serialized = JsonSerializer::Serialize(this, options);
+	File::WriteAllText(ConfigPath, serialized, Encoding::UTF8);
 }
 
 Settings^ Settings::Load()
 {
-    // Create a config if there isn't one
-    if (!File::Exists(ConfigPath))
-        return (gcnew Settings())->InitializeDefaults();
+	// Create a config if there isn't one
+	if (!File::Exists(ConfigPath))
+		return (gcnew Settings())->InitializeDefaults();
 
-    auto serialized = File::ReadAllText(ConfigPath, Encoding::UTF8);
-    Settings^ instance = JsonSerializer::Deserialize<Settings^>(serialized, nullptr);
+	auto serialized = File::ReadAllText(ConfigPath, Encoding::UTF8);
+	auto instance = JsonSerializer::Deserialize<Settings^>(serialized, nullptr);
 
-    // Extra sanitization
-    if (!instance->DllList)
-        instance->DllList = gcnew List<String^>();
+	// Extra sanitization
+	if (!instance->DllList)
+		instance->DllList = gcnew List<String^>();
 
-    // Remove invalid items
-    for each (auto path in instance->DllList->ToArray())
-    {
-        if (!File::Exists(path)) instance->DllList->Remove(path);
-    }
+	// Remove invalid items
+	for each (auto path in instance->DllList->ToArray())
+	{
+		if (!File::Exists(path)) instance->DllList->Remove(path);
+	}
 
-    return instance;
+	return instance;
 }
