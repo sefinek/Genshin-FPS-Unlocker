3927fc10dc506f92fc49fd4232694d570d18cd71
Sefinek <contact@sefinek.net>
2024-09-02 19:18:36 +0200

    Create OPEN THE SOLUTION.cmd

---
+++ OPEN THE SOLUTION.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+set "file_path=D:\Projects\stella\vs\genshin-fps-unlock\unlockfps_nc.sln"
+set "vs_path=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+
+echo Running Genshin Stella Mod made by Sefinek.sln file as administrator... && echo.
+
+powershell.exe -Command "Start-Process -FilePath '%vs_path%' -ArgumentList '\"%file_path%\"' -Verb RunAs"
+
+endlocal
