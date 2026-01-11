c162638e1812f75250d327975a46537dc291e594
Sefinek <contact@sefinek.net>
2026-01-11 16:28:59 +0100

    Migrate to VS2026

--- OPEN THE SOLUTION.cmd
+++ OPEN THE SOLUTION.cmd
@@ -1,11 +1,12 @@
 @echo off
 setlocal
 
-set "file_path=D:\Projects\stella\Genshin-FPS-Unlocker\unlockfps_nc.sln"
-set "vs_path=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+set "file_path=D:\Projects\stella\Genshin-FPS-Unlocker\unlockfps_nc.slnx"
+set "vs_path=C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\devenv.exe"
 
-echo Running unlockfps_nc.sln file as administrator... && echo.
-
-powershell.exe -Command "Start-Process -FilePath '%vs_path%' -ArgumentList '\"%file_path%\"' -Verb RunAs"
+for %%f in ("%file_path%") do (
+    echo Running the '%%~nxf' file as administrator... && echo.
+    powershell.exe -Command "Start-Process '%vs_path%' -ArgumentList '\"%file_path%\"' -Verb RunAs"
+)
 
 endlocal
